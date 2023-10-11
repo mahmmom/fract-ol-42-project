76cec6510043c79b7c2ed135d1bc74545fda3089
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-10-11 13:26:08 +0400

    Update start.c

--- start.c
+++ start.c
@@ -1,37 +1,35 @@
 #include "fractol.h"
-#include "minilibx_macos/mlx.h"
+#include "minilibx-linux/mlx.h"
 
-void	my_mlx_pixel_put(t_fractal *fractol, int x, int y, int color)
+static void    malloc_error(void)
 {
-	char	*dst;
-
-	dst = fractol->addr + (y * fractol->line_length + x * (fractol->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+    perror("Error in malloc allocation");
+    exit(EXIT_FAILURE);
 }
 
-void	ft_kickoff(t_fractal *fractol)
+void	start_fractol(t_fractal *fractol)
 {
 	fractol->mlx = mlx_init();
-	fractol->win = mlx_new_window(fractol->mlx, HEIGHT, WIDTH, "fractol");
-	fractol->img = mlx_new_image(fractol->mlx, HEIGHT, WIDTH);
-	fractol->addr = mlx_get_data_addr(fractol->img, &fractol->bits_per_pixel, 
-		&fractol->line_length, &fractol->endian);
-	fractol->x = 0;
- 	fractol->y = 0;
-	fractol->zoom_factor = 1.5;
-    while (fractol->y < HEIGHT)
+    if (!fractol->mlx)
+        malloc_error();
+	fractol->win = mlx_new_window(fractol->mlx, HEIGHT, WIDTH, fractol->name);
+    if (!fractol->win)
     {
-        fractol->x = 0;
-        while (fractol->x < WIDTH)
-        {
-            double real = ((fractol->x - WIDTH / 2.0) * 4.0 / WIDTH) / fractol->zoom_factor;
-            double imag = ((fractol->y - HEIGHT / 2.0) * 4.0 / HEIGHT) / fractol->zoom_factor;
-
-            my_mlx_pixel_put(fractol, fractol->x, fractol->y, COLOR_BLACK);
-            fractol->x++;
-        }
-        fractol->y++;
+        mlx_destroy_display(fractol->mlx);
+        free(fractol->win);
+        malloc_error();
     }
-	//mlx_key_hook(fractol->win, keyhook, &fractol);
-	mlx_loop(fractol->mlx);
+	fractol->img.img = mlx_new_image(fractol->mlx, HEIGHT, WIDTH);
+    if (!fractol->img.img)
+    {
+        mlx_destroy_window(fractol->mlx, fractol->win);
+        mlx_destroy_display(fractol->mlx);
+        free(fractol->win);
+        malloc_error();
+    }
+	fractol->img.addr = mlx_get_data_addr(fractol->img.img, &fractol->img.bits_per_pixel, 
+		&fractol->img.line_length, &fractol->img.endian);
+	
+	//event_start(fractol);
+    //data_start(fractol);
 }
