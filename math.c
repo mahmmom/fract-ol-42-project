96971ae722f1f352ea1987c88f3770ed1c5aca23
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-10-11 17:02:40 +0400

    Update math.c

--- math.c
+++ math.c
@@ -5,3 +5,21 @@ double  scale(double s, double min2, double max2, double min1, double max1)
 {
     return ((s - min1) * (max2 - min2) / (max1 - min1) + min2);
 }
+
+t_complex   sum_complex(t_complex z1, t_complex z2)
+{
+    t_complex   result;
+
+    result.x = z1.x + z2.x;
+    result.y = z1.y + z2.y;
+    return (result);
+}
+
+t_complex   square_complex(t_complex z)
+{
+    t_complex   result;
+
+    result.x = (z.x * z.x) - (z.y * z.y);
+    result.y = 2*z.x*z.y;
+    return (result);
+}
