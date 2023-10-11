00d1331a9fb6a345eee1211dc236cfec9c94ecf8
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-10-11 17:01:37 +0400

    Update draw.c

--- draw.c
+++ draw.c
@@ -1,5 +1,13 @@
 #include "fractol.h"
 
+static void	my_pixel_put(int x, int y, t_img *img, int color)
+{
+	int	offset;
+
+	offset = (y * img->line_length) + (x * (img->bits_per_pixel / 8));
+	*(unsigned int *)(img->addr + offset) = color;
+}
+
 void    pixel_handel(int x, int y, t_fractal *fractol)
 {
     t_complex   z;
@@ -9,6 +17,15 @@ void    pixel_handel(int x, int y, t_fractal *fractol)
     z.y = 0.0;
     c.x = scale(x, -2, +2, 0, HEIGHT);
     c.y = scale(y, +2, -2, 0, WIDTH);
+    while (??)
+    {
+        z = sum_complex(square_complex(z), c);
+        if ((z.x * z.x) + (z.y * z.y) < fractol->outer_value)
+        {
+        my_pixel_put();
+        return;
+        }
+    }
 }
 
 void    fractal_draw(t_fractal *fractol)
