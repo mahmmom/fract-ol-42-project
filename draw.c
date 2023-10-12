4d9f28e2759d87c0131c3ca485c9052832816408
mahmmom <138707294+mahmmom@users.noreply.github.com>
2023-10-12 10:34:54 +0400

    Update draw.c

--- draw.c
+++ draw.c
@@ -8,24 +8,30 @@ static void	my_pixel_put(int x, int y, t_img *img, int color)
 	*(unsigned int *)(img->addr + offset) = color;
 }
 
-void    pixel_handel(int x, int y, t_fractal *fractol)
+static void    pixel_handel(int x, int y, t_fractal *fractol)
 {
     t_complex   z;
     t_complex   c;
+    int         i;
+    int         color;
 
+    i = 0;
     z.x = 0.0;
     z.y = 0.0;
     c.x = scale(x, -2, +2, 0, HEIGHT);
     c.y = scale(y, +2, -2, 0, WIDTH);
-    while (??)
+    while (i < fractol->iteration)
     {
         z = sum_complex(square_complex(z), c);
         if ((z.x * z.x) + (z.y * z.y) < fractol->outer_value)
         {
-        my_pixel_put();
-        return;
+            color = scale(i, BLACK, WHITE, 0, fractol->iteration);
+            my_pixel_put(x, y, &fractol->img, color);
+            return;
         }
+        i++;
     }
+    my_pixel_put(x, y, &fractol->img, LIME_GREEN);
 }
 
 void    fractal_draw(t_fractal *fractol)
@@ -44,4 +50,6 @@ void    fractal_draw(t_fractal *fractol)
         }
         y++;
     }
+    mlx_put_image_to_window(fractol->mlx, fractol->win,
+        fractol->img.addr, 0, 0);
 }
