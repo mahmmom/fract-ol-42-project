6dab093b6cc5f65bf6e397c3dd07201687aa5ef2
mahmmom <138707294+mahmmom@users.noreply.github.com>
2024-01-23 14:18:24 +0400

    Update zooooooom.c

--- zooooooom.c
+++ zooooooom.c
@@ -16,4 +16,30 @@ void	ft_dezoom(int x, int y, t_fractol *data)
 
 ---------------------------------------------------------------------------------------------
 
-  
+  int	mouse_scroll(int button, int x, int y, t_mlx_info *info)
+{
+	double	mouse_y;
+	double	mouse_x;
+
+	mlx_clear_window(info->mlx_ptr, info->window_ptr);
+	mouse_x = info->xmin
+		+ (x * (info->xmax - info->xmin)) / 800;
+	mouse_y = info->ymin
+		+ (y * (info->ymax - info->ymin)) / HEIGHT;
+	if (button == 4)
+	{
+		info->xmin = mouse_x + (info->xmin - mouse_x) / info->zoom;
+		info->ymin = mouse_y + (info->ymin - mouse_y) / info->zoom;
+		info->xmax = mouse_x + (info->xmax - mouse_x) / info->zoom;
+		info->ymax = mouse_y + (info->ymax - mouse_y) / info->zoom;
+	}
+	if (button == 5)
+	{
+		info->xmin = mouse_x + (info->xmin - mouse_x) * info->zoom;
+		info->ymin = mouse_y + (info->ymin - mouse_y) * info->zoom;
+		info->xmax = mouse_x + (info->xmax - mouse_x) * info->zoom;
+		info->ymax = mouse_y + (info->ymax - mouse_y) * info->zoom;
+	}
+	draw_shape(info);
+	return (info->zoom);
+}
